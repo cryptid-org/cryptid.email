b9ae4add0fc20d574803e462713731c205789b26
Attila Bagossy <bagossyattila@outlook.com>
2018-09-21 23:56:30 +0200

    Added all three API endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,37 @@ const publicParameters = {
         path: '/public-parameters',
         handler(request, h) {
             return 'public parameters';
+        },
+        options: {
+            description: 'Returns the actual public parameters.'
+        }
+    }
+};
+
+const email = {
+    verify: {
+        POST: {
+            method: 'POST',
+            path: '/email/verify',
+            handler(request, h) {
+                return 'Verify email.';
+            },
+            options: {
+                description: 'Initiates the email verification process.'
+            }
+        }
+    }
+};
+
+const privateKey = {
+    POST: {
+        method: 'POST',
+        path: '/private-key',
+        handler(request, h) {
+
+        },
+        options: {
+            description: 'Returns the private key corresponding to the specified public key.'
         }
     }
 };
@@ -11,7 +42,11 @@ const publicParameters = {
 const plugin = {
     name: 'api',
     register(server, options) {
-        server.route(publicParameters.GET);
+        server.route([
+            publicParameters.GET,
+            email.verify.POST,
+            privateKey.POST
+        ]);
     }
 };
 
