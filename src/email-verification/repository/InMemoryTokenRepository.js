9a50bcccb7d1a8f939b97185bdd4b3bde348b695
Attila Bagossy <bagossyattila@outlook.com>
2018-09-22 15:11:47 +0200

    Finished the in memory token repository

--- src/email-verification/repository/InMemoryTokenRepository.js
+++ src/email-verification/repository/InMemoryTokenRepository.js
@@ -2,40 +2,60 @@ const randomstring = require('randomstring');
 
 const config = require('../../../config');
 
+
 const makeInMemoryTokenRepository = function makeInMemoryTokenRepository({ config, randomstring }) {
     const generateFormToken = () => randomstring(config.get('emailVerification.formToken'));
     const generateVerificationToken = () => randomstring(config.get('emailVerification.verificationToken'));
+
+    const storage = new Map();
 
     return {
         async requestFormToken() {
             const token = generateFormToken();
 
-            const result = await setAsync(token, config.get('emailVerification.formToken.placeholder'), 
-                                          'EX', config.get('emailVerification.formToken.placeholderExpiration'));
+            const timeoutHandle = setTimeout(function removeFormToken() {
+                storage.delete(token);
+            }, config.get('emailVerification.formToken.placeholderExpiration') * 1000);
 
-            return result == OK ? token : null;
+            storage.set(token, {
+                verificationToken: config.get('emailVerification.formToken.placeholder'),
+                timeoutHandle
+            });
+
+            return token;
         },
         async requestVerificationToken(formToken) {
-            const placeholderValue = await getAsync(formToken);
+            const value = storage.get(formToken)
 
-            if (placeholderValue != config.get('emailVerification.formToken.placeholder')) {
+            if (!value || value.verificationToken != config.get('emailVerification.formToken.placeholder')) {
                 return null;
             }
 
+            clearTimeout(value.timeoutHandle);
+
             const verificationToken = generateVerificationToken();
 
-            const result = await setAsync(formToken, verificationToken, 'EX', config.get('emailVerification.verificationToken.expiration'));
+            const timeoutHandle = setTimeout(function removeVerificationToken() {
+                storage.delete(formToken);
+            }, config.get('emailVerification.verificationToken.expiration')) * 1000);
 
-            return result == OK ? verificationToken : null;
+            storage.set(token, {
+                verificationToken,
+                timeoutHandle
+            });
+
+            return verificationToken;
         },
         async checkVerificationToken(formToken, verificationToken) {
-            const savedToken = await getAsync(formToken);
+            const saved = storage.get(formToken);
 
-            if (savedToken != verificationToken) {
+            if (!saved || saved.verificationToken != verificationToken) {
                 return false;
             }
 
-            await delAsync(formToken);
+            clearTimeout(saved.timeoutHandle);
+
+            storage.delete(formToken);
 
             return true;
         }
