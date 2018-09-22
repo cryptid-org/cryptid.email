f046028af72f0d3b29bc1dc2933941ed7c8c8ee0
Attila Bagossy <bagossyattila@outlook.com>
2018-09-22 16:39:43 +0200

    The IbeParametersService is now able to properly do key rotation

--- src/ibe-parameters/IbeParametersService.js
+++ src/ibe-parameters/IbeParametersService.js
@@ -1,14 +1,71 @@
+const uuid = require('uuid/v1');
+
 const MetaClient = require('../../ext/metaclient.cjs');
 
 const config = require('../../config');
 
 const { IbeParametersRepository } = require('./repository');
 
-const makeIbeParametersService = function makeIbeParametersService({ config, IbeParametersRepository, MetaClient }) {
-    
+const INSTANTLY = 0;
+const DONE = true;
+
+const makeIbeParametersService = function makeIbeParametersService({ config, IbeParametersRepository, MetaClient, uuid }) {
+    const rotateParametersIn = function rotateParametersIn(ms) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                MetaClient.getInstance()
+                    .then(instance => instance.setup(config.get('ibe.securityLevel')))
+                    .then(result => {
+                        return Object.assign({
+                            id: uuid(),
+                            masterSecret: result.masterSecret,
+                            createdAt: Date.now()
+                        }, result.publicParameters);
+                    })
+                    .then(parameters => IbeParametersRepository.setCurrentParameters(parameters))
+                    .then(() => {
+                        resolve();
+
+                        rotateParametersIn(config.get('ibe.parameterChangeInterval'));
+
+                        return DONE;
+                    });
+            }, ms);
+        });
+    };
+
+    const firstSetup = IbeParametersRepository.getCurrentPublicParameters()
+        .then(pp => {
+            if (pp != null) {
+                const now = Date.now();
+                const timeSince = now - pp.createdAt;
+                const timeUntilExpiry = (createdAt + config.get('ibe.parameterChangeInterval')) - now;
+
+                if (timeSince > config.get('ibe.parameterChangeInterval')) {
+                    return rotateParametersIn(INSTANTLY);
+                }
+
+                rotateParametersIn(timeUntilExpiry);
+
+                return DONE;
+            }
+
+            return rotateParametersIn(INSTANTLY);
+        });
+
+    return {
+        async getCurrentPublicParameters() {
+            await firstSetup;
+            
+            return IbeParametersRepository.getCurrentPublicParameters();
+        },
+        async getParametersForId(parametersId) {
+            return IbeParametersRepository.getParametersForId(parametersId);
+        }
+    };
 };
 
 module.exports = {
     makeIbeParametersService,
-    IbeParametersService = makeIbeParametersService({ config, IbeParametersRepository, MetaClient })
+    IbeParametersService: makeIbeParametersService({ config, IbeParametersRepository, MetaClient, uuid })
 };
